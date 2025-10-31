19185bae3a3bc302411a67c1ed90ddd7f2f3d470
roobak-1234 <roobakdinesh@gmail.com>
2025-11-01 01:04:16 +0530

    Fix API URLs for all services to use production backend

--- src/services/expenseService.js
+++ src/services/expenseService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/expenses';
+const API_URL = process.env.REACT_APP_API_BASE_URL ? `${process.env.REACT_APP_API_BASE_URL}/expenses` : 'http://localhost:8080/api/expenses';
 
 const createAuthenticatedApi = () => {
   const token = localStorage.getItem('token');
